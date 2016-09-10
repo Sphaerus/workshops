f43c9e466ca0f2fa6793768fb6c7f28bc5523b03
Sphaerus <wyfsm1@gmail.com>
2016-09-10 23:45:20 +0200

    refactor

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -6,7 +6,12 @@ class StudentDecorator < BaseDecorator
   def avg_notes(subject_item)
   	subject_item_notes = subject_item.subject_item_notes
   	return "0.00" if subject_item_notes.empty?
-  	avg = Float(subject_item_notes.map(&:value).sum) / Float(subject_item_notes.count)
-  	"%0.2f" % avg
+  	"%0.2f" % average_note(subject_item_notes)
+  end
+
+  private
+
+  def average_note(subject_item_notes)
+  	Float(subject_item_notes.map(&:value).sum) / Float(subject_item_notes.count)
   end
 end
