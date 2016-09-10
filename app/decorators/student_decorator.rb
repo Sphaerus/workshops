12cc58095981bd03161e6f75d9c89a0369a5b85e
Sphaerus <wyfsm1@gmail.com>
2016-09-10 16:09:39 +0200

    student deco spec passes

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,9 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+  	subject_item_notes = subject_item.subject_item_notes
+  	return "0.00" if subject_item_notes.empty?
+  	avg = Float(subject_item_notes.map(&:value).sum) / Float(subject_item_notes.count)
+  	"%0.2f" % avg
   end
 end
