91cddc35297be3ac2e9d57d71f2be06283b54904
Sphaerus <wyfsm1@gmail.com>
2016-09-11 00:43:02 +0200

    student birthday task

--- spec/decorators/student_decorator_spec.rb
+++ spec/decorators/student_decorator_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe StudentDecorator do
   let(:teacher) { create :teacher, first_name: 'John', last_name: 'Smith' }
-  let(:student) { create :student, first_name: 'John', last_name: 'Smith' }
+  let(:student) { create :student, first_name: 'John', last_name: 'Smith', birthday: Date.new(2016,11,9)}
+  let(:student2) { create :student, first_name: 'John', last_name: 'Smith' }
   let(:subject_item) { create :subject_item, teacher: teacher }
   let(:second_subject_item) { create :subject_item }
   let!(:note_1) { create :subject_item_note, value: 5, student: student, subject_item: second_subject_item }
@@ -29,5 +30,23 @@ describe StudentDecorator do
         is_expected.to eq '4.50'
       end
     end
+  end
+
+  describe '#formatted_birthday' do
+    describe 'when student has no birthday filled in' do
+      subject { student2.decorate.formatted_birthday }
+
+      it 'displays empty string' do
+        is_expected.to eq ''
+      end
+    end
+
+    describe 'when student has birthday filled in' do    
+      subject { student.decorate.formatted_birthday }
+      
+      it 'displays string with formatted date of birth' do 
+        is_expected.to eq '2016-11_09'
+      end
+    end  
   end
 end
