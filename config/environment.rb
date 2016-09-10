16ceded2d3c786396f945106ed0d5b356afed308
Sphaerus <wyfsm1@gmail.com>
2016-09-10 14:12:28 +0200

    env

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+ENV['APP_NAME'] = 'RoR Workshops'
 # Initialize the Rails application.
 Rails.application.initialize!
